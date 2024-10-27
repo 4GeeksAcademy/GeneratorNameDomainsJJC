846d051c151b1695a2fd2cfa85c031f5ccca8d5f
JessCP07 <jjcarrera04@gmail.com>
2024-10-27 18:09:36 +0000

    domaincode

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "raccon"];
+  let domains = [".com", ".net", ".us", ".io", ".etc"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          console.log(pronoun + adj + noun + domain);
+        }
+      }
+    }
+  }
 };
